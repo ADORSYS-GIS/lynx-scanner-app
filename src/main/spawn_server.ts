9b52c37a99716e16d48ede05cdb10b797136eb94
Stephane Segning Lambou <selastlambou@gmail.com>
2024-06-30 00:54:01 +0200

    feat: build for mac

--- src/main/spawn_server.ts
+++ src/main/spawn_server.ts
@@ -1,6 +1,8 @@
 import { spawn } from 'node:child_process'
 import { join } from 'node:path'
 import logger from 'electron-log'
+import { app } from 'electron'
+import arch from 'arch'
 
 const serverLog = logger.scope('server')
 
@@ -15,6 +17,9 @@ const serverBinaryPath = join(
   'nativeCompile',
   'lynx-backend'
 )
+
+const appData = app.getPath('userData')
+serverLog.log(`appData: ${appData}`, arch(), buildDownloadUrl('v0.1.0'))
 
 const springApp = spawn(serverBinaryPath, [], {
   env: {
@@ -42,3 +47,32 @@ springApp.on('error', (error) => {
   serverLog.error(error)
 })
 
+function buildDownloadUrl(version: string): string {
+  const os = getOS()
+  const arch = getArch()
+  return `https://github.com/stephane-segning/lynx-scanner-backend/releases/download/${version}/lynx-backend-${os}-${arch}`
+}
+
+function getOS(): string {
+  switch (process.platform) {
+    case 'darwin':
+      return 'macos'
+    case 'win32':
+      return 'windows'
+    case 'linux':
+      return 'linux'
+    default:
+      throw new Error(`Unsupported OS: ${process.platform}`)
+  }
+}
+
+function getArch(): string {
+  switch (arch()) {
+    case 'arm64':
+      return 'arm64'
+    case 'x64':
+      return 'amd64'
+    default:
+      throw new Error(`Unsupported arch: ${arch()}`)
+  }
+}

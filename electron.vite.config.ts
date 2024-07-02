6e2fe4801ef794fcbd7a320e1759e1035a6aa443
Koufan-De-King <kingkoufan@gmail.com>
2024-07-02 09:34:25 +0100

    set environmental variable for windows platform

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  build: {
-    module: 'node16',
-  },
   main: {
     plugins: [externalizeDepsPlugin()]
   },
